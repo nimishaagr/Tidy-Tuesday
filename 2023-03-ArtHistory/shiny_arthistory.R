925ee23ba3842deb67ad45156ce93066db913b3a
Nimisha Agrawal <21362459+nimishaagr@users.noreply.github.com>
2023-07-02 08:36:54 +0530

    Update shiny_arthistory.R

--- 2023-03-ArtHistory/shiny_arthistory.R
+++ 2023-03-ArtHistory/shiny_arthistory.R
@@ -1,6 +1,5 @@
 ###############################################################################
 #Shiny
-source("arthistory.R")
 
 if(!require(shiny, quietly=T)) install.packages("shiny", repos = "https://cloud.r-project.org/")
 library(shiny)
@@ -8,6 +7,10 @@ if(!require(shinydashboard, quietly=T)) install.packages("shinydashboard", repos
 library(shinydashboard, quietly=T)
 if(!require(shinyWidgets, quietly=T)) install.packages("shinyWidgets", repos = "https://cloud.r-project.org/")
 library(shinyWidgets, quietly=T)
+if(!require(rsconnect, quietly=T)) install.packages("rsconnect", repos = "https://cloud.r-project.org/")
+library(rsconnect, quietly=T)
+
+source("arthistory.R")
 
 header <- dashboardHeader(title="Diversity in American art history discourse", titleWidth = 450)
 
@@ -25,26 +28,36 @@ body <- dashboardBody(
             fluidRow(
               tabBox(id="tab_nationality", width=12, height=800,
                      tabPanel("Exhibtions",
+                              prettyRadioButtons(
+                                inputId = "exbn_selector1a",
+                                label = "Choose Exhibition", 
+                                choices = c("MoMA"="moma_count_to_year", 
+                                            "Whitney"="whitney_count_to_year"),
+                                inline = TRUE, 
+                                status = "success",
+                                fill = TRUE,
+                                selected = "moma_count_to_year"
+                              ),
                               plotOutput("plot1a")),
                      tabPanel("Page area",
                               dropdownButton(
-                                tags$h3("List of Inputs"),
-                                
-                                selectInput("bookSelector",
+                                tags$h4("List of Inputs"),
+                                br(),
+                                selectInput("book_selector1b",
                                             "Select Book",
                                             choices = c("All" = "All",
                                                         "History of Art (H. W. Janson)" = "Janson",
                                                         "Art Through the Ages (Helen Gardner)" = "Gardner"),
                                             selected = "All"),
                                 
-                                sliderInput(inputId = 'n_nationalities',
+                                sliderInput(inputId = 'n_nationalities1b',
                                             label = 'Nationalities: Show Top ___',
                                             value = 35,
                                             min = 5,
                                             max = 50),
                                 
-                                prettySwitch(inputId = "show_artists",
-                                             label = "Show top 20 artists' names",
+                                prettySwitch(inputId = "show_artists1b",
+                                             label = "Show top 50 artists.",
                                              value = T,
                                              status = "success",
                                              fill = TRUE),
@@ -56,6 +69,25 @@ body <- dashboardBody(
                               ),
                               plotOutput("plot1b")),
                      tabPanel("Frequency",
+                              dropdownButton(
+                                sliderInput(inputId = 'n_nationalities1c',
+                                            label = 'Nationalities: Show Top ___',
+                                            value = 8,
+                                            min = 5,
+                                            max = 50),
+                                
+                                circle = TRUE, status = "danger",
+                                icon = icon("gear"), width = "300px",
+                                
+                                tooltip = tooltipOptions(title = "Click to modify graph parameters!")
+                              ),
+                              br(),
+                              selectInput("book_selector1c",
+                                          "Select Book",
+                                          choices = c("All" = "All",
+                                                      "History of Art (H. W. Janson)" = "Janson",
+                                                      "Art Through the Ages (Helen Gardner)" = "Gardner"),
+                                          selected = "All"),
                               plotOutput("plot1c"))
               )
             )
@@ -64,10 +96,47 @@ body <- dashboardBody(
             fluidRow(
               tabBox(id="tab_gender", width=12, height=800,
                      tabPanel("Exhibtions",
+                              prettyRadioButtons(
+                                inputId = "exbn_selector2a",
+                                label = "Choose Exhibition", 
+                                choices = c("MoMA"="moma_count_to_year", 
+                                            "Whitney"="whitney_count_to_year"),
+                                inline = TRUE, 
+                                status = "success",
+                                fill = TRUE,
+                                selected = "moma_count_to_year"
+                              ),
                               plotOutput("plot2a")),
                      tabPanel("Page area",
+                              dropdownButton(
+                                tags$h4("List of Inputs"),
+                                br(),
+                                selectInput("book_selector2b",
+                                            "Select Book",
+                                            choices = c("All" = "All",
+                                                        "History of Art (H. W. Janson)" = "Janson",
+                                                        "Art Through the Ages (Helen Gardner)" = "Gardner"),
+                                            selected = "All"),
+                                
+                                prettySwitch(inputId = "show_artists2b",
+                                             label = "Show top 50 artists.",
+                                             value = T,
+                                             status = "success",
+                                             fill = TRUE),
+                                
+                                circle = TRUE, status = "danger",
+                                icon = icon("gear"), width = "300px",
+                                
+                                tooltip = tooltipOptions(title = "Click to modify graph parameters!")
+                              ),
                               plotOutput("plot2b")),
                      tabPanel("Frequency",
+                              selectInput("book_selector2c",
+                                          "Select Book",
+                                          choices = c("All" = "All",
+                                                      "History of Art (H. W. Janson)" = "Janson",
+                                                      "Art Through the Ages (Helen Gardner)" = "Gardner"),
+                                          selected = "All"),
                               plotOutput("plot2c"))
               )
             )
@@ -76,10 +145,47 @@ body <- dashboardBody(
             fluidRow(
               tabBox(id="tab_race", width=12, height=800,
                      tabPanel("Exhibtions",
+                              prettyRadioButtons(
+                                inputId = "exbn_selector3a",
+                                label = "Choose Exhibition", 
+                                choices = c("MoMA"="moma_count_to_year", 
+                                            "Whitney"="whitney_count_to_year"),
+                                inline = TRUE, 
+                                status = "success",
+                                fill = TRUE,
+                                selected = "moma_count_to_year"
+                              ),
                               plotOutput("plot3a")),
                      tabPanel("Page area",
+                              dropdownButton(
+                                tags$h4("List of Inputs"),
+                                br(),
+                                selectInput("book_selector3b",
+                                            "Select Book",
+                                            choices = c("All" = "All",
+                                                        "History of Art (H. W. Janson)" = "Janson",
+                                                        "Art Through the Ages (Helen Gardner)" = "Gardner"),
+                                            selected = "All"),
+                                
+                                prettySwitch(inputId = "show_artists3b",
+                                             label = "Show top 50 artists.",
+                                             value = T,
+                                             status = "success",
+                                             fill = TRUE),
+                                
+                                circle = TRUE, status = "danger",
+                                icon = icon("gear"), width = "300px",
+                                
+                                tooltip = tooltipOptions(title = "Click to modify graph parameters!")
+                              ),
                               plotOutput("plot3b")),
                      tabPanel("Frequency",
+                              selectInput("book_selector3c",
+                                          "Select Book",
+                                          choices = c("All" = "All",
+                                                      "History of Art (H. W. Janson)" = "Janson",
+                                                      "Art Through the Ages (Helen Gardner)" = "Gardner"),
+                                          selected = "All"),
                               plotOutput("plot3c"))
               )
             )
@@ -89,93 +195,33 @@ body <- dashboardBody(
 ui <- dashboardPage(header, sidebar, body, skin="black")
 
 server <- function(input,output) {
+  output$plot1a <- renderPlot({
+    plot1a_render(input$exbn_selector1a)
+  }, height=650)
+  output$plot2a <- renderPlot({
+    plot2a_render(input$exbn_selector2a)
+  }, height=650)
+  output$plot3a <- renderPlot({
+    plot3a_render(input$exbn_selector3a)
+  }, height=650)
   output$plot1b <- renderPlot({
-    
-  })
+    plot1b_render(input$book_selector1b,input$n_nationalities1b,input$show_artists1b)
+  }, height=650)
+  output$plot2b <- renderPlot({
+    plot2b_render(input$book_selector2b,input$show_artists2b)
+  }, height=650)
+  output$plot3b <- renderPlot({
+    plot3b_render(input$book_selector3b,input$show_artists3b)
+  }, height=650)
+  output$plot1c <- renderPlot({
+    plot1c_render(input$book_selector1c,input$n_nationalities1c)
+  }, height=600)
+  output$plot2c <- renderPlot({
+    plot2c_render(input$book_selector2c)
+  }, height=300)
+  output$plot3c <- renderPlot({
+    plot3c_render(input$book_selector3c)
+  }, height=400)
 }
 
 shinyApp(ui, server)
-
-
-#     tabItem(tabName = "nationality",
-#       fluidRow(
-#         box(
-#           width=8,
-#           title="Select Book",
-#           status = "warning", solidHeader = TRUE,
-#           selectInput("bookSelector",
-#                       "", 
-#                       choices = c(
-#                         "All" = "All",
-#                         "History of Art (H. W. Janson)" = "Janson",
-#                         "Art Through the Ages (Helen Gardner)" = "Gardner"),
-#                       selected = "All"),
-#           "Horst Woldemar Janson (1913–1982), was a Russian Empire-born German-American professor of art history.", br(),
-#           "Helen Gardner (1878–1946) was an American art historian and educator.", br(), 
-#           "Their respective books have many editions over the years and have remained a standard text for American art history classes."
-#           )
-#         ),
-#       fluidRow(
-#         box(plotOutput("plot1a"), width=5,
-#             title="How many times do artists of different nationalities feature in the book(s) across the years?",
-#             status = "primary", solidHeader = TRUE
-#             ),
-#         box(plotOutput("plot1b"), width=7,
-#             title="Average space per page given to different artists across editions",
-#             status = "primary", solidHeader = TRUE
-#             )
-#         )
-#       )
-#     )
-#   )
-# 
-# server <- function(input, output) { 
-#    
-#   output$plot1a <- renderPlot({
-#     art_nation_count <- arthistory |>
-#       filter(if (input$bookSelector!="All") {book==input$bookSelector} else {TRUE}, 
-#              artist_nationality_other!="Other") |>
-#       mutate(year=year(year)) |>
-#       group_by(artist_nationality_other, year) |>
-#       summarise(feature_count=n()) |>
-#       ungroup() 
-#     
-#     art_nation_count |>
-#       ggplot(aes(x=year, y=feature_count, colour=artist_nationality_other)) +
-#       geom_line(linewidth=1) +
-#       geom_point(size=1) +
-#       geom_label_repel(aes(label = artist_nationality_other),
-#                        data = art_nation_count |> filter(year == max(year)),
-#                        size = 4) +
-#       theme_linedraw() +
-#       labs(y="Count",
-#            x="Textbook Edition Year") +
-#       theme(legend.position = "none")
-#   })
-#   
-#   output$plot1b <- renderPlot({
-#     art_nation_space <- arthistory |>
-#       filter(if (input$bookSelector!="All") {book==input$bookSelector} else {TRUE}, 
-#              !str_detect(artist_name,"N/A")) |>
-#       group_by(artist_nationality_other, artist_name) |>
-#       summarise(space_ratio=mean(space_ratio_per_page_total)) |>
-#       ungroup() 
-#     
-#     top_10_artists <- art_nation_space |>
-#       group_by(artist_nationality_other) |>
-#       slice_max(space_ratio, n=10)
-#     
-#     art_nation_space |>
-#       ggplot(aes(area=space_ratio, 
-#                  subgroup=artist_nationality_other,
-#                  fill=artist_nationality_other,
-#                  label=ifelse(artist_name %in% (top_10_artists$artist_name),artist_name, ""))
-#       ) +
-#       geom_treemap() +
-#       geom_treemap_subgroup_text(place = "centre", size=12, grow = F, alpha = 0.5, fontface = "bold") +
-#       geom_treemap_text(fontface = "italic", place = "centre", size=5, grow=F, reflow=T) +
-#       theme(legend.position="none") +
-#       scale_fill_brewer(palette = "Pastel1")
-#   })
-# }
-
